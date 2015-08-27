11b56513327de7a65819abbc0fb71ec207f72ddd
smg219 <smg219@lehigh.edu>
2015-08-27 15:28:16 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////
+//Scott Galdi
+//Demo 0
+//Hello WOrld Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+  public static void main(String[] args) {
+
+
+//  print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2015!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+  }
+}
