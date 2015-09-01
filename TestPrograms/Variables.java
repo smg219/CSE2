2361e5b06e0a885e3a437c718c3fd97178a92063
smg219 <smg219@lehigh.edu>
2015-09-01 20:57:47 +0000

    updated WelcomeClass

---
+++ TestPrograms/Variables.java
@@ -0,0 +1,26 @@
+public class Variables
+{
+    
+        /////////////////////////////////
+        //
+        //  Desription of method
+        //
+        /////////////////////////////////
+        public static void main(String[] args)
+        {
+            // TODD Auto-generated method stub
+            
+            float processorSpeed;
+            byte numberProcessors;
+            float computerSpeed;
+            
+            processorSpeed = 2.3f;
+            numberProcessors = 4;
+            
+            computerSpeed = processorSpeed * numberProcessors;
+            
+            System.out.println("Computer Speed =" + computerSpeed);
+            
+        }
+        
+}
---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////////
+//Scott Galdi
+//hw01
+//Welcome Class Java Program
+//
+
+//  the class
+public class WelcomeClass{
+    
+//  the main method    
+  public static void main(String[] args) {
+      
+//  Displays the message shown below when program is run  
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-S--M--G--2--1--9-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer I visited Bermuda with my family. While we were there we visited the beaches and the crystal caves.");
+    
+  }
+}
